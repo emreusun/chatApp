ea39a23679f0e2dbbd8b2773035b9e1facfb9b93
emreusun <emre95neo@gmail.com>
2023-01-11 17:37:16 -0500

    express fixed

--- app.js
+++ app.js
@@ -2,16 +2,25 @@ const express = require('express'); // import express
 const app = express(); // create an express app
 const http = require('http'); // import the node server package
 const server = http.createServer(app); // use our app file with the server
+// add in the socket io stuff
+const { Server } = require("socket.io");
+const io = new Server(server);
 
 
 const port = process.env.PORT || 3000;
 
-// this is a route handler -> şisten for incoming requests and sned back a reponse
+// this is a route handler -> listen for incoming requests and send back a reponse
 app.get('/', (req, res) => {
-  res.send('<h1>Hello world</h1>');
+  res.sendFile(__dirname + '/index.html');
 });
 
 // set up the server to listen for incoming connections at this port
-server.listen(3000, () => {
-  console.log('listening on ${port}');
+server.listen(port, () => {
+  console.log(`listening on ${port}`);
+});
+
+// socket.io connection
+
+io.on('connection', (socket) => {
+  console.log('chat user connected' , socket);
 });
